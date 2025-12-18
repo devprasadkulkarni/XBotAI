ea5803a5ac20ceb7e2d69c6740648b3bb6c4c6a4
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-12-18 15:02:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import History from "./components/History";
+
+const App = () => {
+    return;
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+    </Routes>;
+};
+
+export default App;
