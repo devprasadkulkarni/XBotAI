6115a37d7a3e51d7cea2d588dac22227888ca16a
Prasad Kulkarni <dev.prasad.kulkarni@gmail.com>
2025-12-20 10:14:07 +0530

    feat:conversations ui structure

--- src/components/Conversations.jsx
+++ src/components/Conversations.jsx
@@ -1,18 +1,39 @@
 import logo from "../assets/logo.svg";
 
 const Conversations = () => {
+    const questions = [
+        "Hi, what is the weather",
+        "Hi, what is my location",
+        "Hi, what is the temperature",
+        "Hi, how are you",
+    ];
+
     return (
-        <div className="bg-[#D7C7F4]">
+        <div className="bg-[#f5f2fb] w-[80vw] h-screen">
             <div className="flex flex-col">
                 <div>
-                    <h1>Bot AI</h1>
+                    <h1 className="text-3xl font-bold text-[#9785BA]">
+                        Bot AI
+                    </h1>
                 </div>
                 <div>
                     <h2>How Can I Help You Today?</h2>
                     <img src={logo} alt="logo" />
                 </div>
+                <div className="grid grid-cols-2 gap-5">
+                    {questions.map((question) => (
+                        <div key={question} className=" h-[10vh] bg-white">
+                            <div>
+                                <p>{question}</p>
+                                <p>Get immediate AI generated response</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
                 <div>
-                    
+                    <input className="p-3 bg-white rounded-md" type="text" />
+                    <button className="px-3 py-2 bg-[#d7c7f4]">Ask</button>
+                    <button className="px-3 py-2 bg-[#d7c7f4]">Save</button>
                 </div>
             </div>
         </div>
--- src/components/NewChat.jsx
+++ src/components/NewChat.jsx
@@ -3,14 +3,14 @@ import { TbEdit } from "react-icons/tb";
 
 const NewChat = () => {
     return (
-        <div className="w-80">
-            <div className="flex bg-[#9785BA] justify-between items-center p-5 ">
+        <div className="w-[15vw] h-screen ">
+            <div className="flex bg-[#d7c7f4] justify-between items-center p-3 ">
                 <img src={logo_square} alt="logo" />
                 <h2 className="text-2xl">New Chat</h2>
                 <TbEdit size={30} />
             </div>
             <div className="flex justify-center items-center">
-                <button className="mt-5 rounded-md px-5 py-3 text-xl  bg-[#9785BA]">
+                <button className="mt-5 rounded-md px-5 py-3 text-xl  bg-[#d7c7f4]">
                     Past Conversations
                 </button>
             </div>
